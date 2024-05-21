9722c0810127b9e02256f184ff85e634afe4d11d
kerlonfernandes <kerlon1221@gmail.com>
2024-05-21 17:28:15 -0300

    first part is ok

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -1,8 +1,13 @@
 <?php
-require_once("./_app/Configurations.php"); 
-require_once("./_app/functions.php"); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
 
+use HelpersClass\Helpers;
 
+define('BASE_PATH', dirname(__DIR__) . '/');
+require_once(BASE_PATH . '_app/Configurations.php');
 class PagesController extends RenderView {
 
     public function index(){
--- views/home.php
+++ views/home.php
@@ -5,10 +5,28 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $title ?></title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="<?= SITE ?>/public/css/home.css?id=<?= uniqid() ?>">
+  <link rel="stylesheet" href="<?= SITE ?>/public/assets/bootstrap.min.css?id=<?= uniqid() ?>">
 </head>
 
-<body>
+<body class="home">
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-9">
+        <div class="card card-block p-5">
+          
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="card card-block" style="height: 100%;">
 
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
